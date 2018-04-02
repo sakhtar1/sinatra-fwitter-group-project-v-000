50445f84c03a129de7a22111d348565e4af39f82
Sharmina Akhtar <sakht318@gmail.com>
2018-04-02 21:16:47 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,7 @@
 ActiveRecord::Schema.define(version: 20180401190338) do
 
   create_table "tweets", force: :cascade do |t|
-    t.string  "content"
-    t.integer "user_id"
+    t.string "content"
   end
 
   create_table "users", force: :cascade do |t|
