bc3bf36ae299fa27cda0ca41265525cd109534e9
Sharmina Akhtar <sakht318@gmail.com>
2018-04-02 21:17:41 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180401190338) do
-
-  create_table "tweets", force: :cascade do |t|
-    t.string "content"
-  end
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.string "password_digest"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
